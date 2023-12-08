96f90866c800a7f9f56a7d69cdde99bd120a2157
ReinaldoMantovani <reinaldotha88@gmail.com>
2023-12-08 09:57:14 -0300

    Project Created

---
+++ src/main/java/br/com/reinaldo/StudySpringBoot3/StudySpringBoot3Application.java
@@ -0,0 +1,13 @@
+package br.com.reinaldo.StudySpringBoot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBoot3Application.class, args);
+	}
+
+}
