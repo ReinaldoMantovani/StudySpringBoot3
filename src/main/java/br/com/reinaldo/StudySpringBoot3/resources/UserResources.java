6461cdd7fb1ed428f8e742ec284e1c7e02afdc16
ReinaldoMantovani <reinaldotha88@gmail.com>
2023-12-08 12:25:46 -0300

    Service layer, component registration

--- src/main/java/br/com/reinaldo/StudySpringBoot3/resources/UserResources.java
+++ src/main/java/br/com/reinaldo/StudySpringBoot3/resources/UserResources.java
@@ -1,19 +1,35 @@
 package br.com.reinaldo.StudySpringBoot3.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.reinaldo.StudySpringBoot3.entities.User;
+import br.com.reinaldo.StudySpringBoot3.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L,"Reinaldo","reinaldo@gmail.com","999999","12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
