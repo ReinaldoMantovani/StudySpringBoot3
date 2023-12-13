1be1df0b04843273fc3f76c4f662ab7213cf91fd
ReinaldoMantovani <reinaldotha88@gmail.com>
2023-12-13 16:15:28 -0300

    Exception handling - delete

--- src/main/java/br/com/reinaldo/StudySpringBoot3/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/reinaldo/StudySpringBoot3/resources/exceptions/ResourceExceptionHandler.java
@@ -15,8 +15,18 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e , HttpServletRequest request ) {
-		String error = "Rsource not found!";
+		String error = "Resource not found!";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getLocalizedMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> database(DatabaseException e , HttpServletRequest request ) {
+		String error = "Database error!";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getLocalizedMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(err);
--- src/main/java/br/com/reinaldo/StudySpringBoot3/services/UserService.java
+++ src/main/java/br/com/reinaldo/StudySpringBoot3/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.reinaldo.StudySpringBoot3.entities.User;
 import br.com.reinaldo.StudySpringBoot3.repositories.UserRepository;
+import br.com.reinaldo.StudySpringBoot3.resources.exceptions.DatabaseException;
 import br.com.reinaldo.StudySpringBoot3.services.exceptions.ResourceNotFoundException;
 
 
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);			
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
